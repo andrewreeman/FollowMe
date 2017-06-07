45c882cbccd06977def59248ddf68ffbd18e04a1
Andrew <andrew@andrews-macbook-air.local>
2017-06-07 19:00:04 +0100

    Issue #4: created location tracking interaction for controlling the state of tracking

---
+++ FollowMe/LocationTrackingInteractor.swift
@@ -0,0 +1,18 @@
+//
+//  LocationTrackingInteractor.swift
+//  FollowMe
+//
+//  Created by Andrew on 07/06/2017.
+//  Copyright © 2017 Stepwise. All rights reserved.
+//
+
+import Foundation
+
+
+/**
+ This class will contain the state for if tracking is turned on or off.
+ If the state changes it will update its listeners
+*/
+class LocationTrackingInteractor {
+    
+}
